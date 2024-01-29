17255dd11af5fae569c003a735443eb18b91da17
Stephannie M <rumbi.m@hotmail.co.uk>
2024-01-29 12:11:48 +0000

    display addition questiosn to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,16 +13,29 @@ for (let button of buttons){
         } else {
          // if the submit button is not clicked it will dispaly the other game types (+,-,/)
             let gameType = this.getAttribute("data-type");
-            alert(`You clicked ${gameType}`);
+            runGame(gameType);
+
         }
     });
 }
+
+runGame("addition")
+
+
+
+
 });
 
 // creating to random number between 1 and 25 (floor rounds down)
-function runGame(){
+function runGame(gameType){
     let num1=Math.floor(Math.random()* 25) + 1;
     let num2=Math.floor(Math.random()* 25) + 1;
+
+    if (gameType=="addition"){displayAdditionQuestion(num1, num2)
+    } else {
+        alert(`unknown game type: ${gameType}`);
+        throw `unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 
@@ -38,7 +51,12 @@ function incrementScore(){}
 function incrementWrongAnswer(){}
 
 
-function displayAdditionQuestion(){}
+function displayAdditionQuestion(operand1, operand2){
+    document.getElementById('operand1').textContent=operand1;
+    document.getElementById('operand2').textContent=operand2;
+    document.getElementById('operator').textContent="+";
+
+}
 
 
 function displaySubtractQuestion(){}
