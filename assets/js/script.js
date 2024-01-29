43e4f0960ee5a362d5ff7e68ce04ca4fe81474ab
Stephannie M <rumbi.m@hotmail.co.uk>
2024-01-29 13:56:15 +0000

    added code for division

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         });
     }
-    // If the enter key is pressed it call the check answer funtion 
+    // If the enter key is pressed it calls the check answer funtion 
     document.getElementById("answer-box").addEventListener("keydown", function (event) {
         if (event.key === "Enter") {
             checkAnswer();
@@ -31,8 +31,10 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function runGame(gameType) {
     // empties the anwser box and adds cursor
+
     document.getElementById("answer-box").value = "";
-    document.getElementsById("answer-box").focus();
+    document.getElementById("answer-box").focus();
+
     // creating to random number between 1 and 25 (floor rounds down)
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
@@ -43,7 +45,9 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion(num1, num2);
-    } else {
+    } else if (gameType === "division"){
+        displayDivisionQuestion(num1, num2);
+    }else {
         alert(`unknown game type: ${gameType}`);
         throw `unknown game type: ${gameType}. Aborting!`;
     }
@@ -51,6 +55,7 @@ function runGame(gameType) {
 
 
 }
+
 
 /**
  * Checks the answer against the first elemnt on the retured calculate
@@ -91,6 +96,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -136,4 +143,9 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 
-function displayDivisionQuestion() { }
+function displayDivisionQuestion(operand1, operand2) {
+    // by multiplying the op1 and op 2 allows the op1 to be a higher number than op 2
+    document.getElementById('operand1').textContent = operand1 * operand2;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "/";
+ }
